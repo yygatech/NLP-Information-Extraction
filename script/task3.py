81bc3fb53d6b3f35019b0889a63d74948d911d85
yygatech <yygatech@gmail.com>
2018-12-08 22:21:58 -0600

    adjust structure

--- script/task3.py
+++ script/task3.py
@@ -10,6 +10,8 @@
 # download directory: /usr/local/share/nltk_data
 # download what you will need
 
+import pickle
+
 import task2
 import task3_1
 import task3_3
@@ -18,10 +20,16 @@ import task3_4
 import task3_5
 
 # load corpus
-# corpus = task2._load_corpus("../data/corpus.txt")
+corpus = task2._load_corpus("../data/corpus.txt")
 
 ## 3.1
-# sentences, words_all = task3_1._task3_1(corpus)
+sentences, words_all = task3_1._task3_1(corpus)
+
+with open(r'../pickle/sentences.pickle', 'wb') as output:
+    pickle.dump(sentences, output)
+
+with open(r'../pickle/words_all.pickle', 'wb') as output:
+    pickle.dump(words_all, output)
 
 # TEST
 # print("sentences")
@@ -31,20 +39,29 @@ import task3_5
 # print(words_all[1:2])
 
 ## 3.3
-# pos_tags_all = task3_3._pos_tag(words_all)
+pos_tags_all = task3_3._pos_tag(words_all)
+
+with open(r'../pickle/pos_tags_all.pickle', 'wb') as output:
+    pickle.dump(pos_tags_all, output)
 
 # TEST
 # print("pos_tags_all")
 # print(pos_tags_all[1:2])
 
-# wn_tags_all = task3_3._wn_tag(pos_tags_all)
+wn_tags_all = task3_3._wn_tag(pos_tags_all)
+
+with open(r'../pickle/wn_tags_all.pickle', 'wb') as output:
+    pickle.dump(wn_tags_all, output)
 
 # TEST
 # print("wn_tags_all")
 # print(wn_tags_all[1:2])
 
 ## 3.2
-# lemmas_all = task3_2._lemmatize(words_all, wn_tags_all)
+lemmas_all = task3_2._lemmatize(words_all, wn_tags_all)
+
+with open(r'../pickle/lemmas_all.pickle', 'wb') as output:
+    pickle.dump(lemmas_all, output)
 
 # TEST
 # print("lemmas_all")
@@ -66,16 +83,16 @@ import task3_5
 #     parse_tree.pretty_print()
 
 ## 3.5
-synsets = task3_5.__synsets("canis")
-synset = synsets[0]
-hypernyms = task3_5.__hypernymns(synset)
-hyponyms = task3_5.__hyponyms(synset)
-meronyms = task3_5.__meronyms(synset)
-holonyms = task3_5.__holonyms(synset)
+# synsets = task3_5.__synsets("canis")
+# synset = synsets[0]
+# hypernyms = task3_5.__hypernymns(synset)
+# hyponyms = task3_5.__hyponyms(synset)
+# meronyms = task3_5.__meronyms(synset)
+# holonyms = task3_5.__holonyms(synset)
 
 # TEST
-print("synsets:", synsets, "\n")
-print("hypernyms:", hypernyms, "\n")
-print("hyponyms:", hyponyms, "\n")
-print("meronyms(being a member of):", meronyms, "\n")
-print("holonyms(having members of):", holonyms, "\n")
+# print("synsets:", synsets, "\n")
+# print("hypernyms:", hypernyms, "\n")
+# print("hyponyms:", hyponyms, "\n")
+# print("meronyms(being a member of):", meronyms, "\n")
+# print("holonyms(having members of):", holonyms, "\n")
